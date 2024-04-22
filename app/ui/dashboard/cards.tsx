524f904a20f6f46155f99ee6b6f8f36ffe67afe4
aakansh15 <iamaakanshanand@gmail.com>
2024-04-22 14:05:09 +0530

    Search and Pagination working

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,10 +15,12 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const totalPaidInvoices = 0;
-  const totalPendingInvoices = 0;
-  const numberOfInvoices = 0;
-  const numberOfCustomers = 0;
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
