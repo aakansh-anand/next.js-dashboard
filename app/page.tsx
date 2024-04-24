27243da15caa5ae92bd7cfd461ad3ec515981fa9
aakansh15 <iamaakanshanand@gmail.com>
2024-04-24 12:38:18 +0530

    Improving Accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from "./ui/home.module.css"
 import Image from 'next/image';
 
 export default function Page() {
